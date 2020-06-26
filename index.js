4d5415ca5fb736b261f4efd739770929f43eca14
vladyslav223 <vladyslav223@gmail.com>
2020-06-26 13:40:26 +0300

    working with db

--- index.js
+++ index.js
@@ -1,47 +1,82 @@
 "use strict";
 const express = require("express");
 const bodyParser = require("body-parser");
-var goods = require('./goods');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.port || 4444;
 
-app.get("/", (req, res) => {
-    res.send("Hello API")
-});
+const goodScheme = new Schema({
+    productName: {
+        type: String,
+        required: true,
+        default: "NoProductName",
+        unique: true,
+    },
+    salePrice: {
+        type: Number,
+        default: null,
+    }
+},
+    { versionKey: false }
+);
 
-app.get("/goods", (req, res) => {
-    res.send(goods)
+mongoose.connect("mongodb://localhost:27017/goods", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Good = mongoose.model("Good", goodScheme);
+
+app.get("/goods", (req, res) => {    
+    Good.find()
+    .then((good) => {
+        console.log("Найден объект", good);
+        res.send(good);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
 });
 
 app.get("/goods/:id", (req, res) => {
-    let good = goods.find((good) => good.id === req.params.id)
-    res.send(good);
+    Good.findById(req.params.id)
+    .then((good) => {
+        console.log("Найден объект" + "___" + req.params.id, good);
+        res.send(good);
+    })
+    .catch(function (err) {
+        console.log(err);
+    }); 
 });  
 
 app.post("/goods", (req, res) => {
-    console.log(req.body);
-    const good = {
-        "id": Date.now(),
-        "productName": req.body.productName,                 
-    }
-    goods.push(good);
-    res.send(good);
+    Good.create(req.body)
+    .then((good) => {
+        console.log("Создан объект", good);
+        res.send(req.body);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
 });
 
 app.put("/goods/:id", (req, res) => {
-    let good = goods.find((good) => good.id === req.params.id);
-    good.productName = req.body.productName;
-    res.send(good);
-});  
+    Good.findByIdAndUpdate(req.params.id, req.body)
+    .then((good) => {
+        console.log("Изменен объект", req.body);
+        res.send(good);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+});
 
 app.listen(port, err => {
-  if (err) {
-    return console.log("ERROR", err);
-}
-  console.log(`Listening on port ${port}`);
+    if (err) {
+        return console.log("ERROR", err);
+    }
+    console.log(`Listening on port ${port}`);
 });
